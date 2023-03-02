77e04bbfb7e92a925c79abff01d224342c912824
Ryan Gildea <ryangildea@gmail.com>
2023-03-01 20:24:05 -0500

    added persistence to localStorage

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -3,18 +3,52 @@ const input = document.getElementById("todoinput")! as HTMLButtonElement;
 const form = document.querySelector("form")!;
 const list = document.getElementById("todolist")!;
 
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON == null) return [];
+  return JSON.parse(todosJSON);
+}
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("SUBMITTED!");
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  todos.push(newTodo);
 
-  const newTodoText = input.value;
+  saveTodos(todos);
+  createTodo(newTodo);
+
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
   const newLI = document.createElement("LI");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  newLI.append(newTodoText);
-  list.append(newLI);
+  checkbox.checked = todo.completed;
+
+  checkbox.addEventListener("change", () => {
+    todo.completed = checkbox.checked;
+    saveTodos(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  });
+
+  newLI.append(todo.text);
   newLI.append(checkbox);
-  input.value = "";
+  list.append(newLI);
+}
+
+function saveTodos(todos: Todo[]) {
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 form.addEventListener("submit", handleSubmit);
